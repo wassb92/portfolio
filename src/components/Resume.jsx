6b9c3b5a0342e83dbe24c49ce6d53b2593918b2b
Bouzidi Wassini <wassini.bouzidi@epitech.eu>
2022-12-04 23:12:51 +0100

    [ADD] (Resume.jsx): seeking as parameter

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,12 +5,13 @@ import resumeFreelancePng from "assets/resume/freelance.png";
 import resumeFreelancePdf from "assets/resume/freelance.pdf";
 
 const DownloadResume = (props) => {
+  const seekingTrad = props?.seeking === "freelance" ? "freelance" : "stage";
   return (
     <div className="inline-flex space-x-10 p-10 hover:scale-125">
       <a
         href={props.file}
         className="text-2xl p-4 font-semibold text-white rounded-xl transition-all duration-500 bg-gradient-to-br to-secondary via-black from-main bg-size-200 hover:bg-right-bottom"
-        download="wassini_bouzidi_cv"
+        download={`wassini_bouzidi_${seekingTrad}_cv`}
       >
         Télécharger en {props.ext}
       </a>
@@ -18,31 +19,25 @@ const DownloadResume = (props) => {
   );
 };
 
-const Resume = () => {
+const Resume = ({ seeking }) => {
   return (
     <div className="w-full h-full text-center bg-gradient-to-br from-main to-secondary">
       <div className="flex justify-center p-4 md:px-80">
         <img
-          src={
-            global.current === "freelance"
-              ? resumeFreelancePng
-              : resumeInternPng
-          }
+          src={seeking === "freelance" ? resumeFreelancePng : resumeInternPng}
           className="rounded-xl object-cover w-full h-full"
           alt="resume"
         />
       </div>
       <DownloadResume
         ext="PDF"
-        file={
-          global.current === "freelance" ? resumeFreelancePdf : resumeInternPdf
-        }
+        file={seeking === "freelance" ? resumeFreelancePdf : resumeInternPdf}
+        seeking={seeking}
       />
       <DownloadResume
         ext="PNG"
-        file={
-          global.current === "freelance" ? resumeFreelancePng : resumeInternPng
-        }
+        file={seeking === "freelance" ? resumeFreelancePng : resumeInternPng}
+        seeking={seeking}
       />
     </div>
   );
